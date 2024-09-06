77d61d2fa8013049b7b9f0201e3cdc57cccc3eb3
Zoya774 <zoya.rasonline@gmail.com>
2024-09-06 14:58:46 +0530

    fontchangecommit

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react';
-import haridwarlogo from '../../assets/haridwarlogo.png';
+import React, { useState, useEffect, useRef } from "react";
+import haridwarlogo from "../../assets/haridwarlogo.png";
 import logo from "../../assets/redefinelogo.png";
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
@@ -11,21 +11,21 @@ const Navbar = () => {
   const menuRef = useRef(null); // Ref to track clicks outside the menu
   const location = useLocation();
   const navigate = useNavigate();
-  
+
   // Scroll to section
   const scrollToSection = (id) => {
     const section = document.getElementById(id);
     if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
+      section.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   // Check the current URL and scroll to the section on load
   useEffect(() => {
-    if (location.hash === '#projects') {
-      scrollToSection('projects');
-    } else if (location.hash === '#achievements') {
-      scrollToSection('achievements');
+    if (location.hash === "#projects") {
+      scrollToSection("projects");
+    } else if (location.hash === "#achievements") {
+      scrollToSection("achievements");
     }
   }, [location]);
 
@@ -39,9 +39,9 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', changeBackground);
+    window.addEventListener("scroll", changeBackground);
     return () => {
-      window.removeEventListener('scroll', changeBackground);
+      window.removeEventListener("scroll", changeBackground);
     };
   }, []);
 
@@ -54,12 +54,12 @@ const Navbar = () => {
 
   useEffect(() => {
     if (showMenu) {
-      document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener("mousedown", handleClickOutside);
     } else {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     }
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [showMenu]);
 
@@ -78,9 +78,9 @@ const Navbar = () => {
 
   // Handle navigation to homepage with specific sections
   const handleNavigateWithSection = (sectionId) => {
-    if (location.pathname !== '/') {
+    if (location.pathname !== "/") {
       // Navigate to home first, and then scroll to section
-      navigate('/');
+      navigate("/");
       setTimeout(() => scrollToSection(sectionId), 100); // Delay for smooth scroll after navigation
     } else {
       scrollToSection(sectionId); // If already on the homepage, just scroll
@@ -90,7 +90,7 @@ const Navbar = () => {
   return (
     <nav
       className={`fixed top-0 left-0 w-full z-50 transition-colors duration-500 ${
-        navbar ? 'bg-gray-900 shadow-md opacity-75' : 'bg-transparent'
+        navbar ? "bg-gray-900 shadow-md opacity-75" : "bg-transparent"
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-24">
@@ -101,8 +101,18 @@ const Navbar = () => {
 
         {/* Desktop Navigation */}
         <div className="hidden md:flex space-x-8">
-          <Link to={'/'} className="text-gray-100 hover:text-gray-300 transition-colors">Home</Link>
-          <Link to={'/aboutus'} className="text-gray-100 hover:text-gray-300 transition-colors">About Us</Link>
+          <Link
+            to={"/"}
+            className="text-gray-100 hover:text-primary transition-colors"
+          >
+            Home
+          </Link>
+          <Link
+            to={"/aboutus"}
+            className="text-gray-100 hover:text-primary transition-colors"
+          >
+            About Us
+          </Link>
 
           {/* Projects Dropdown */}
           <div
@@ -111,47 +121,65 @@ const Navbar = () => {
             onMouseLeave={handleMouseLeave}
           >
             <span
-              className="text-gray-100 hover:text-gray-300 transition-colors cursor-pointer"
-              onClick={() => handleNavigateWithSection('projects')}
+              className="text-gray-100 hover:text-primary transition-colors cursor-pointer"
+              onClick={() => handleNavigateWithSection("projects")}
             >
               Projects
             </span>
             {showSubmenu && (
-              <div className="absolute top-full mt-2 w-48 bg-gray-800 rounded-md shadow-lg transition-opacity duration-300 ease-in-out opacity-100">
+              <div className="absolute top-full mt-2 w-48 bg-gray-800 rounded-md shadow-lg transition-opacity duration-300 ease-in-out opacity-100 border-t-2 border-primary">
                 <Link
-                  to={'/haridwarparadise'}
+                  to={"/haridwarparadise"}
                   target="_blank"
-                  className="block px-4 py-2 text-gray-100 hover:bg-gray-700 transition-all duration-200"
+                  className="block px-4 py-2 text-gray-100 hover:bg-gray-700 hover:text-primary transition-all duration-200"
                 >
                   Haridwar Paradise
                 </Link>
                 <Link
-                  to={'/devprayagparadise'}
+                  to={"/devprayagparadise"}
                   target="_blank"
-                  className="block px-4 py-2 text-gray-100 hover:bg-gray-700 transition-all duration-200"
+                  className="block px-4 py-2 text-gray-100 hover:bg-gray-700 hover:text-primary transition-all duration-200"
                 >
                   Devprayag Paradise
                 </Link>
+                {/* Add more dropdown links here */}
               </div>
             )}
           </div>
 
           <span
-            className="text-gray-100 hover:text-gray-300 transition-colors cursor-pointer"
-            onClick={() => handleNavigateWithSection('achievements')}
+            className="text-gray-100 hover:text-primary transition-colors cursor-pointer"
+            onClick={() => handleNavigateWithSection("achievements")}
           >
             Achievements
           </span>
-          <Link to={'/contactus'} className="text-gray-100 hover:text-gray-300 transition-colors">
+          <Link
+            to={"/contactus"}
+            className="text-gray-100 hover:text-primary transition-colors"
+          >
             Contact Us
           </Link>
         </div>
 
         {/* Mobile Menu Button */}
         <div className="md:hidden">
-          <button onClick={() => setShowMenu(!showMenu)} className="text-gray-100 focus:outline-none">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          <button
+            onClick={() => setShowMenu(!showMenu)}
+            className="text-gray-100 focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
             </svg>
           </button>
         </div>
@@ -161,19 +189,19 @@ const Navbar = () => {
       <div
         ref={menuRef}
         className={`md:hidden fixed top-0 right-0 h-full w-64 bg-gray-900 z-40 transform transition-transform duration-300 ease-in-out ${
-          showMenu ? 'translate-x-0' : 'translate-x-full'
+          showMenu ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <div className="p-4">
           <Link
-            to={'/'}
+            to={"/"}
             className="block px-4 py-2 text-gray-100 hover:bg-gray-700"
             onClick={() => setShowMenu(false)}
           >
             Home
           </Link>
           <Link
-            to={'/aboutus'}
+            to={"/aboutus"}
             className="block px-4 py-2 text-gray-100 hover:bg-gray-700"
             onClick={() => setShowMenu(false)}
           >
@@ -183,7 +211,7 @@ const Navbar = () => {
             className="block px-4 py-2 text-gray-100 hover:bg-gray-700 cursor-pointer"
             onClick={() => {
               setShowMenu(false);
-              handleNavigateWithSection('projects');
+              handleNavigateWithSection("projects");
             }}
           >
             Projects
@@ -192,13 +220,13 @@ const Navbar = () => {
             className="block px-4 py-2 text-gray-100 hover:bg-gray-700 cursor-pointer"
             onClick={() => {
               setShowMenu(false);
-              handleNavigateWithSection('achievements');
+              handleNavigateWithSection("achievements");
             }}
           >
             Achievements
           </span>
           <Link
-            to={'/contactus'}
+            to={"/contactus"}
             className="block px-4 py-2 text-gray-100 hover:bg-gray-700"
             onClick={() => setShowMenu(false)}
           >
